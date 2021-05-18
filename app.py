7859a6eaf196ab03904c56d3ffba3b271bb78156
shivam <batrashiv98@gmail.com>
2021-05-18 14:09:39 +0530

    third commit (added opencv)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask , render_template , redirect , url_for
+from flask import Flask , render_template , redirect , url_for ,Response
+from camera import VideoCamera
 
 app = Flask(__name__)
 
@@ -11,6 +12,16 @@ def home() :
 def detection() :
     return render_template('detection.html' , title='Detection')
 
+def gen(camera):
+    while True:
+        #get camera frame
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__' :
 
